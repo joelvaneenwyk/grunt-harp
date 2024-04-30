65480c442982b4c5afd5e04ce1ca4fa2c33aa155
Joel Van Eenwyk <joel.vaneenwyk@gmail.com>
2024-04-29 19:28:32 -0700

    improvement: rearrange ESLint dependencies and remove deprecated rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,6 +5,7 @@
 import js from "@eslint/js";
 import globals from "globals";
 
+/** @type { import("eslint").Linter.FlatConfig[] } */
 export default [
     js.configs.recommended,
     {
@@ -19,48 +20,16 @@ export default [
             }
         },
         rules: {
-            // enable additional rules
-            "indent": [
-                "error",
-                4
-            ],
-            "linebreak-style": "off",
             "func-names": "off",
-            "max-len": [
-                "error",
-                {
-                    "code": 120
-                }
-            ],
-            "quotes": [
-                "error",
-                "double"
-            ],
-            "semi": [
-                "error",
-                "always"
-            ],
             "arrow-body-style": [
                 "error",
                 "as-needed"
             ],
             "prefer-arrow-callback": "off",
-            // override default options for rules from base configurations
             "no-cond-assign": [
                 "error",
                 "always"
             ],
-            "comma-dangle": [
-                "error",
-                {
-                    "arrays": "never",
-                    "objects": "never",
-                    "imports": "never",
-                    "exports": "never",
-                    "functions": "never"
-                }
-            ],
-            // disable rules from base configurations
             "no-console": "off"
         }
     }
